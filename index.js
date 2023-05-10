568e87a25c6fbf9652786234bd43f898c3273bde
aleksei-commmune <ivanov.aleksei@commmune.jp>
2023-05-10 17:06:03 +0900

    more logs

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import core from "@actions/core";
 import github from "@actions/github";
 
+const isAdd = (set) => set === "true";
+const isRemove = (set) => !isAdd(set);
+
 try {
   const oktokit = github.getOctokit(process.env.GITHUB_TOKEN);
   const set = core.getInput("set");
   const label = core.getInput("label");
+
+  console.log(`set: ${set}`, `label: "${label}"`);
 
   const prNumber = github.context.payload.pull_request.number;
 
@@ -19,14 +24,18 @@ try {
     const hasLabel = labels.data.some((l) => l.name === label);
     console.log(
       hasLabel
-        ? `PR #${prNumber} has label ${label}; proceeding...`
-        : `PR #${prNumber} does not have label ${label}; skip...`
+        ? `PR #${prNumber} has label ${label}; ${
+            isRemove(set) ? "proceeding..." : "skip..."
+          }`
+        : `PR #${prNumber} does not have label ${label}; ${
+            isAdd(set) ? "proceeding..." : "skip..."
+          }`
     );
     return labels.data.some((l) => l.name === label);
   };
 
   const removeLabel = async () => {
-    console.log(`Removing label ${label} from PR #${prNumber}`);
+    console.log(`Removing label "${label}" from PR #${prNumber}`);
     if (await hasLabel()) {
       await oktokit.request(
         "DELETE /repos/{owner}/{repo}/issues/{issue_number}/labels/{label}",
@@ -40,7 +49,7 @@ try {
   };
 
   const addLabel = async () => {
-    console.log(`Adding label ${label} to PR #${prNumber}`);
+    console.log(`Adding label "${label}" to PR #${prNumber}`);
     if (!(await hasLabel())) {
       await oktokit.request(
         "POST /repos/{owner}/{repo}/issues/{issue_number}/labels",
