ea1acb966a85a478f7f3eca35f0f531d47c7a9ae
aleksei-commmune <ivanov.aleksei@commmune.jp>
2023-05-10 16:49:57 +0900

    add logs

--- index.js
+++ index.js
@@ -16,10 +16,17 @@ try {
         issue_number: prNumber,
       }
     );
+    const hasLabel = labels.data.some((l) => l.name === label);
+    console.log(
+      hasLabel
+        ? `PR #${prNumber} has label ${label}; proceeding...`
+        : `PR #${prNumber} does not have label ${label}; skip...`
+    );
     return labels.data.some((l) => l.name === label);
   };
 
   const removeLabel = async () => {
+    console.log(`Removing label ${label} from PR #${prNumber}`);
     if (await hasLabel()) {
       await oktokit.request(
         "DELETE /repos/{owner}/{repo}/issues/{issue_number}/labels/{label}",
@@ -33,6 +40,7 @@ try {
   };
 
   const addLabel = async () => {
+    console.log(`Adding label ${label} to PR #${prNumber}`);
     if (!(await hasLabel())) {
       await oktokit.request(
         "POST /repos/{owner}/{repo}/issues/{issue_number}/labels",
